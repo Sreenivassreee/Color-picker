6efaa9df16e98d7ab435c89389fcb38f00ff864f
Sreenivas K <ksreenivascse@gmail.com>
2020-08-10 11:17:31 +0530

    Initial Commit

---
+++ Color Picker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Color Picker
+//
+//  Created by Sreenivas K on 10/08/20.
+//  Copyright © 2020 Sreenivas K. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
