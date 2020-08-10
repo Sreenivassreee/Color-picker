54285263952d34febe30f8a1eb01007d001be02d
Sreenivas K <ksreenivascse@gmail.com>
2020-08-10 12:27:51 +0530

    Inital Commit

--- Color Picker/ContentView.swift
+++ Color Picker/ContentView.swift
@@ -9,8 +9,24 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var red:Double=225;
+    @State private var green:Double=225;
+    @State private var blue:Double=225;
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            Rectangle().frame(width: 150, height: 150, alignment: Alignment.center)
+                .foregroundColor(Color(red: red/225, green: green/225, blue: blue/225))
+            
+            Slider(value: $red,in:0...225,step: 1)
+            Text("Red: \(Int(red))")
+            
+            Slider(value: $green,in:0...225,step: 1)
+            Text("Green: \(Int(green))")
+            
+            Slider(value: $blue,in:0...225,step: 1)
+            Text("Blue: \(Int(blue))")
+        }.padding()
     }
 }
 
